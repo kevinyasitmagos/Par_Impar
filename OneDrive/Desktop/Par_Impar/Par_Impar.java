abb547643f65482179d9eb6302277ddf1ac20d0b
kevinyasitmagos <20253RD021@utez.edu.mx>
2026-01-23 12:37:24 -0600

    Cree la clase Par_Impar en VScode

---
+++ OneDrive/Desktop/Par_Impar/Par_Impar.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+ 
+ public class Par_Impar{
+    public static main void (String [] args){
+         
+         Scanner sc = new Scanner(System.in);
+         int numero 
+    }
+ }
