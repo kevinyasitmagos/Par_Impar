252611d9be6d3a793d3deacc288c7f46f8fee8ab
kevinyasitmagos <20253RD021@utez.edu.mx>
2026-01-27 15:44:26 -0600

    Cree la clase Usuario

---
+++ OneDrive/Desktop/Usuario/Usuario.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Usuario{
+public static void main(String []args){
+Scanner sc = new Scanner(System.in);
+
+String usuario = "Yasit";
+String contraseña = "101010"
+}
+}
