d3df1bfca0a632be20eaeef278a8d4f48a65045a
kevinyasitmagos <20253RD021@utez.edu.mx>
2026-01-28 12:36:18 -0600

    Cree la clase ciclos

--- OneDrive/Desktop/Usuario/Usuario.java
+++ OneDrive/Desktop/Usuario/Usuario.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 
-public class Usuario{
-public static void main(String []args){
-Scanner sc = new Scanner(System.in);
+public class Usuario {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-String usuariop = "Yasit";
-String contrap = "101010";
-  
-  boolean acceso = false;
+        String usuariop = "Yasit";
+        String contrap = "101010";
 
-  System.out.println("Ingresa el usuario");
-  String usuario = sc.nextLine();
+        boolean acceso = false;
 
-  System.out.println("Ingresa la contraseña");
-  String contra = sc.nextLine();
+        System.out.println("Ingresa el usuario");
+        String usuario = sc.nextLine();
 
-    if (usuario && contra == contrap && contrap) {
-        boolean acceso = true;
-        } 
-        
-        If (acceso = true) {
+        System.out.println("Ingresa la contraseña");
+        String contra = sc.nextLine();
+
+        if (usuario.equals(usuariop) && contra.equals(contrap)) {
+            acceso = true;
+        }
+
+        if (acceso == true) {
             System.out.println("Acceso permitido");
         } else {
             System.out.println("Acceso denegado");
         }
-        }
+    }
 }
---
+++ OneDrive/Desktop/l/Ciclos.java
@@ -0,0 +1,16 @@
+public class Ciclos{
+    public static void main(String [] args){
+        //while
+        int i = 0;
+
+        //while como contador 
+        while (i <= 20) {
+         if (i<=20 && i%2==1) {
+            System.out.print("Los numeros pares menores a 20 son:" + i);
+            i++;
+         }
+        }
+   
+
+    }
+}
